def9e052a27226b5800694200403d5d3956ac73f
Blendhasani <bh51257@ubt-uni.net>
2024-11-28 21:26:55 +0100

    Add project files.

---
+++ PulseDonor.Core/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace PulseDonor.Core
+{
+	public class Class1
+	{
+
+	}
+}
---
+++ PulseDonor.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PulseDonor.MVC.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
