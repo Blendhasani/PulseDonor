0d5a28f30c307f9918f9893a51aa80325f94caa5
Blendhasani <bh51257@ubt-uni.net>
2024-12-25 20:30:15 +0100

    structure for the user crud mvc

--- PulseDonor.MVC/Controllers/UserController.cs
+++ PulseDonor.MVC/Controllers/UserController.cs
@@ -23,34 +23,45 @@ namespace PulseDonor.MVC.Controllers
 			return Json(result);
 		}
 
-	//	[HttpGet]
-	//	public IActionResult GetTableData()
-	//	{
-	//		var data = new List<object>
-	//{
-	//	new { OrderID = "0006-3629", CarMake = "Land Rover", CarModel = "Range Rover", Color = "Orange", DepositPaid = "22672.60", OrderDate = "2016-11-28" },
-	//	new { OrderID = "66403-315", CarMake = "GMC", CarModel = "Jimmy", Color = "Goldenrod", DepositPaid = "55141.29", OrderDate = "2017-04-29" },
-	//	new { OrderID = "54868-5055", CarMake = "Ford", CarModel = "Club Wagon", Color = "Goldenrod", DepositPaid = "70991.52", OrderDate = "2017-03-16" }
-	//};
+		public IActionResult Add()
+		{
+			return View();
+		}
+		public IActionResult Edit()
+		{
+			return View();
+		}
 
-	//		// Return the data as JSON as-is won't work with KTDatatable:
-	//		// return Json(data);
 
-	//		// Instead, wrap it in the structure expected by KTDatatable.
-	//		var result = new
-	//		{
-	//			meta = new
-	//			{
-	//				page = 1, // current page
-	//				pages = 1, // total number of pages
-	//				perpage = 10, // number of records per page
-	//				total = data.Count, // total number of records
-	//			},
-	//			data = data
-	//		};
 
-	//		return Json(result);
-	//	}
+		//	[HttpGet]
+		//	public IActionResult GetTableData()
+		//	{
+		//		var data = new List<object>
+		//{
+		//	new { OrderID = "0006-3629", CarMake = "Land Rover", CarModel = "Range Rover", Color = "Orange", DepositPaid = "22672.60", OrderDate = "2016-11-28" },
+		//	new { OrderID = "66403-315", CarMake = "GMC", CarModel = "Jimmy", Color = "Goldenrod", DepositPaid = "55141.29", OrderDate = "2017-04-29" },
+		//	new { OrderID = "54868-5055", CarMake = "Ford", CarModel = "Club Wagon", Color = "Goldenrod", DepositPaid = "70991.52", OrderDate = "2017-03-16" }
+		//};
+
+		//		// Return the data as JSON as-is won't work with KTDatatable:
+		//		// return Json(data);
+
+		//		// Instead, wrap it in the structure expected by KTDatatable.
+		//		var result = new
+		//		{
+		//			meta = new
+		//			{
+		//				page = 1, // current page
+		//				pages = 1, // total number of pages
+		//				perpage = 10, // number of records per page
+		//				total = data.Count, // total number of records
+		//			},
+		//			data = data
+		//		};
+
+		//		return Json(result);
+		//	}
 
 	}
 }
